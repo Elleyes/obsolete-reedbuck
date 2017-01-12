e3da455c32eb027b4ebc0d3e79701c429d5d0553
elleyes <ymirandilla@gmail.com>
2017-01-12 15:13:40 -0800

    fixed minor error with task file name

---
+++ postgres-express-react-node-tutorial/components/Todo.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Button from './Button'
+import Task from './Task'
+
+export default class Todo extends Component {
+
+  render () {
+    const {todo, tasks, onTaskChange, onCompleteToggle, onDelete, movePriorityDown, movePriorityUp } = this.props
+    const {id, completed} = todo
+    const taskIsStriked = completed?
+    <Task
+      tasks={tasks}
+      className={'completed'}
+      todo={todo}
+      onTaskChange={onTaskChange}
+    />
+    :
+    <Task
+      tasks={tasks}
+      className={'uncompleted'}
+      todo={todo}
+      onTaskChange={onTaskChange}
+    />
+
+    let reorderButtons = null
+
+    if( todo.id === tasks[0].id ){
+      reorderButtons =
+        <td>
+        <Button icon={"glyphicon-arrow-down"} onClick={(e) => movePriorityDown(e, todo.id-1, tasks)}/>
+        </td>
+    } else if ( todo.id === tasks[tasks.length-1].id ) {
+      reorderButtons =
+      <td>
+      <Button icon={"glyphicon-arrow-up"} onClick={ (e) => movePriorityUp(e, todo.id-1, tasks) }/>
+      </td>
+    } else {
+      reorderButtons =
+      <td>
+      <Button icon={"glyphicon-arrow-up"} onClick={(e) => movePriorityUp(e, todo.id-1, tasks) }/>
+      <br/>
+      <Button icon={"glyphicon-arrow-down"} onClick={(e) => movePriorityDown(e, todo.id-1, tasks) }/>
+      </td>
+    }
+
+    return (
+            <tr id={id} className='table-row'>
+            <td>
+            <Button icon={"glyphicon-ok"} onClick={onCompleteToggle}/>
+            </td>
+              {taskIsStriked}
+            {reorderButtons}
+            <td>
+              <Button icon={"glyphicon-remove"} onClick={onDelete}/>
+            </td>
+            </tr>
+    )
+  }
+}
